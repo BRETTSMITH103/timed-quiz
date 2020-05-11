b72bf6e943b7ea6621846411a8e8e71254c6f06c
Brett Smith <brettsmith.rbc2020@gmail.com>
2020-05-11 15:28:44 -0400

    questions and timer still not working

---
+++ assets/questions.js
@@ -0,0 +1,52 @@
+var questions = [
+  {
+    questions: "What is Arnold Schwarzenegger's first movie?",
+    choices: ["Hercules in New York", "Die Hard", "Terminator", "True Lies"],
+    answer: "Hercules in New York"
+  },
+  {
+    questions: "What is Arnold's name in Kindergarten Cop ?",
+    choices: ["John Kimble", "Crisp", "John McClain", "Alan Schaefer"],
+    answer: "John Kimble"
+  },
+  {
+    questions: "How many times did he win Mr.Olympia?",
+    choices: ["Two", "Six", "Seven", "Five"],
+    answer: "Seven"
+  },
+  {
+    questions: "What movie is GET TO THA CHAPPA from?",
+    choices: ["True Lies", "Terminator 2", "Predator", "The Expendables"],
+    answer: "Predator"
+  },
+  {
+    questions: "What year was he elected to Governor of California?",
+    choices: ["2003", "1999", "1984", "2007"],
+    answer: "2003"
+  },
+  {
+    questions: "What country is he from?",
+    choices: ["Australia", "Germany", "Austria", "Canada"],
+    answer: "Austria"
+  },
+  {
+    questions: "Who did he have an sffair with?",
+    choices: ["Maid", "Assistant", "Co-star", "Mechanic"],
+    answer: "Maid"
+  },
+  {
+    questions: "What was his largest grossing movie as the lead actor",
+    choices: ["Terminator 2", "Total Recall", "The Expendables", "Predator"],
+    answer: "Noun"
+  },
+  {
+    questions: "What role did he play as in Batman & Robin?",
+    choices: ["The Penguin", "Robin", "Poison Ivy", "Mr.Freeze"],
+    answer: "git push origin master"
+  },
+  {
+    questions: "Is there such thing as a bad Arnold impression?",
+    choices: ["No", "Leave it to Arnold", "My uncle Jim", "I dont like anything fun"],
+    answer: "No"
+  }
+]
--- assets/script.js
+++ assets/script.js
@@ -1,155 +1,108 @@
-// CREATE OUR VARIABLES
+var timeLeftEl = document.querySelector("#time-left");
+var startScreenEl = document.querySelector("#start-screen");
+var startGameBtnEl = document.querySelector("#start-game-btn");
+var quizContentEl = document.querySelector("#quiz-content");
+var postGameEl = document.querySelector("#post-game-screen");
+var userScoreEl = document.querySelector("#user-score");
+var playAgainBtnEl = document.querySelector("#play-again-btn");
 
-// HTML ELEMENTS
-var $firstNumber = $('#first-number');
-var $secondNumber = $('#second-number');
-var $operator = $('#operator');
-var $result = $('#result');
+var currentQuestionIndex = 0;
 
-// VARIABLES FOR CALCULATOR LOGIC
-var firstNumber = '';
-var secondNumber = '';
-var operator = '';
-var result = '';
+var timerIntervalId;
+var score = 0;
 
-// create a function for clearing all data and resetting values
-function calculatorReset() {
-  // reset logic variables
-  // ex. firstNumber = "";
-  // clear out HTML element values
-  // ex. $firstNumber.text("");
-}
+var secondsLeft = questions.length * 6;
+var intervalRunning = false
 
-// function to handle button click for numbers
-function handleNumberClick(event) {
-  // read value of number picked
-  var numberPicked = $(this).val(){
+function startGame() {
+  secondsLeft = questions.length * 6;
 
-    if (operator === "") {
+  timeLeftEl.textContent = secondsLeft;
 
+  userScoreEl.value = userScoreEl.defaultValue;
 
+  if (!intervalRunning) {
+    intervalRunning = true;
+    timerIntervalId = setInterval(function () {
+      secondsLeft--;
+      timeLeftEl.textContent = secondsLeft
+      console.log(secondsLeft)
+      if (secondsLeft === 0) {
 
+        stopGame();
+      }
+    }, 1000);
   }
 
-
-}
-  // var numberPicked = $(event.target).val();
-  // if there is nothing set in the operator variable yet, add numberPicked to firstNumber
-  // else, add numberPicked to secondNumber
-  // write number to the proper location on the page
+  displayQuestions(0);
 }
 
-// function to handle operator selection
-function handleOperatorClick(event) {
-  // get operator value out of button clicked
+function displayQuestions(questionIndex) {
+  if (questionIndex === questions.length) {
 
-  car operatorPicked = $(this).val();
-  // var operatorPicked = $(event.target).val()
-  // if firstNumber variable is empty, return false
-  // create variables to reference all DOM elements we're working with
-
-  // time-left
-
-  // start game button
-  // start-screen element
-
-  // quiz-content
-
-  // post-game-screen
-  // user-score
-  // play-again-btn
-
-
-  // create variables for game logic
-  // timerIntervalId
-  // score
-  // secondsLeft
-
-
-  // create function to start game
-  // set secondsLeft variable starting time (300 seconds = 5 minutes)
-  // write secondsLeft to the page
-
-  // reset score to 0
-  // write score to the page (optional)
-
-  // hide start-screen element && post-game-screen
-  // show quiz-content element
-
-  // display first question
-  displayQuestion(0);
-
-  // set timerIntervalId to setInterval function that decrements secondsLeft every second
-  timerIntervalId = setInterval(function () {
-    secondsLeft--;
-    if (secondsLeft <= 0) {
-      stopGame();
-    }
-  }, 1000);
-
-
-
-  // create function to display a question and possible choices
-  function displayQuestion(questionIndex) {
-    // check if questionIndex in questions array doesn't exist
-    if (!questions[questionIndex]) {
-      // stop game, we've hit last question
-      return stopGame();
-    }
-
-    // get questions[questionIndex]
-    // print question to the page
-    // use data attribute to know which index the question is
-    // loop through choices and print out choices to the page (make them buttons)
+    console.log('hi')
+    return stopGame();
 
   }
+  var currentQuestion = questions[questionIndex];
 
-// create function to handle users answering
-  // use event delegation to make sure button was clicked
-  // read data attribute of what question we answered (index)
-  // check to see if choice picked is same as questions correct answer
-  // if yes, increase score++
-  // if no, subtract time from secondsLeft
+  var titleEl = document.getElementById("question-title");
 
-  // get index of next question (this question's index + 1)
-  // run displayQuestion(nextQuestionIndex)
+  titleEl.textContent = currentQuestion.questions;
 
+  var choicesEl = document.getElementById("choices");
+  choicesEl.innerHTML = "";
+  currentQuestion.choices.forEach(function (choice, i) {
+    var choiceButton = document.createElement("button");
+    choiceButton.setAttribute("value", choice);
+    choiceButton.textContent = i + 1 + ". " + choice;
+    choiceButton.onclick = questionClick;
+    choicesEl.appendChild(choiceButton);
+  });
 
+}
 
-// create a function to stop the game (either by answering all the questions or time has run out)
-  // clearInterval() to stop the timer
-  // hide quiz-content element
-  // show post-game-screen
-  // print out user score
-
-
-
-// add event listeners
-  // start game button (for starting the game)
-  // quizcontent (for answering a question) -> use event delegation
-  // play again button (for starting the game)
-
-
-  if (!firstNumber) {
-    return false;
-
+function questionClick() {
+  if (this.value !== questions[currentQuestionIndex].answer) {
+    alert("Nope! Wrong Answer!");
+  } else {
+    alert("You're Right! Way to go!");
+    score++;
   }
-  // else set operator variable to value of operatorPicked
-  // write operator value to the page
+  currentQuestionIndex++;
+  displayQuestions(currentQuestionIndex);
 }
 
-// function to handle clicking the equal sign "="
-function handleEqualClick(event) {
-  // check to make sure that firstNumber, operator, and secondNumber all have values
-  // if any of them don't have a value, return false
-  // use if statements to check value of "operator" variable and perform math operation based on that
-  // if (operator === "plus") { }
-  // write total to the page
+function stopGame() {
+  console.log("stopGame");
+  intervalRunning = false;
+  clearInterval(timerIntervalId)
+  quizContentEl.setAttribute("class", "hide");
+  postGameEl.removeAttribute("class");
+  userScoreEl.textContent = score;
 }
 
-// create event listeners
-$('.number').on('click', handleNumberClick);
-$('.operator').on('click', handleOperatorClick);
-$('.equal').on('click', handleEqualClick);
-$('.clear').on('click', calculatorReset);
-Collapse
+function playAgain() {
+  postGameEl.setAttribute("class", "hide");
+  quizContentEl.removeAttribute("class");
+
+  startGame();
+}
+
+startGameBtnEl.addEventListener("click", function (event) {
+  var element = event.target;
+  if (element.matches("button") === true) {
+    startScreenEl.setAttribute("class", "hide");
+
+    quizContentEl.removeAttribute("class");
+    startGame();
+  }
+});
+
+startGameBtnEl.addEventListener("click", startGame);
+
+quizContentEl.addEventListener("click", function (event) {
+  event.preventDefault();
+});
+
+playAgainBtnEl.addEventListener("click", playAgain);
